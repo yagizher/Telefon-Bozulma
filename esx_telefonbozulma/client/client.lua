a34cbc0aef1a3dee524963dd5a0b252870892a47
Yagiz ALP <yagizwasmeow@gmail.com>
2020-04-30 16:10:40 +0300

    Update client.lua

--- esx_telefonbozulma/client/client.lua
+++ esx_telefonbozulma/client/client.lua
@@ -1,15 +1,9 @@
+ESX = nil
+
 local yuzenoyuncu = PlayerPedId()
-
-
 local telefon = true         -- EĞER TELEFONUNUZUN BOZULMASINI İSTEMİYORSANIZ 'false' YAPIN.
-
-
 local karapara = true        -- EĞER KARA PARA'LARINIZIN ISLANMASINI/BOZULMASINI İSTEMİYORSANIZ 'false' YAPIN.
-
-
 local nakitpara = true       -- EĞER NAKİT PARA'LARINIZIN ISLANMASINI/BOZULMASINI İSTEMİYORSANIZ 'false' YAPIN.
-
-
 
 Citizen.CreateThread(function()
   while ESX == nil do
@@ -27,9 +21,9 @@ Citizen.CreateThread(function()
 		  TriggerServerEvent('esx_telefonbozulma:telefon')
 		   if karapara == true then
 		  TriggerServerEvent('esx_telefonbozulma:karapara')
-		   end
-		  end
-      end
-    end
-    end
-  end)
+					end
+				end
+			end
+		end
+	end
+end)		
